e58c7fc4adaa22e3f9272bce4070a52d679fff97
Oleg Glingeanu <oleg.glingeanu@gmail.com>
2022-10-01 04:25:45 +0100

    Yeah
    Yeah

---
+++ js/script.js
@@ -0,0 +1,40 @@
+// JavaScript Document
+
+//paste this code under head tag or in a seperate js file.
+	// Wait for window load
+$(window).load(function() {
+		// Animate loader off screen
+		$(".se-pre-con").fadeOut("slow");;
+		
+
+            function onScrollInit( items, trigger ) {
+                items.each( function() {
+                var osElement = $(this),
+                    osAnimationClass = osElement.attr('data-os-animation'),
+                    osAnimationDelay = osElement.attr('data-os-animation-delay');
+                  
+                    osElement.css({
+                        '-webkit-animation-delay':  osAnimationDelay,
+                        '-moz-animation-delay':     osAnimationDelay,
+                        'animation-delay':          osAnimationDelay
+                    });
+
+                    var osTrigger = ( trigger ) ? trigger : osElement;
+                    
+                    osTrigger.waypoint(function() {
+                        osElement.addClass('animated').addClass(osAnimationClass);
+                        },{
+                            triggerOnce: true,
+                            offset: '75%'
+                    });
+                });
+            }
+
+            onScrollInit( $('.os-animation') );
+            onScrollInit( $('.staggered-animation'), $('.staggered-animation-container') 
+		
+		
+		
+);
+
+});
